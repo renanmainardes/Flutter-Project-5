6447a6be859489694dd65cd7b1a85d850c3d1734
renanmainardes <renanmainardes@outlook.com>
2022-11-17 20:36:18 -0300

    "novo teste"

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
        
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Teste'),
     );
   }
 }
